820836440496bc32aee5a84f67ff6838bf46ea3b
zsheng <zsheng.dev@gmail.com>
2026-02-04 17:50:45 +0800

    Add basic search

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [tailwind()],
+  build: {
+    format: "file",
+  },
+  integrations: [tailwind(), pagefind()],
 });
